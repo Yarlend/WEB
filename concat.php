c00c360ec2c42e9e7374941f964a32204989daa4
Yarlend <sadovoy1yaroslav@gmail.com>
2024-09-26 14:46:25 +0300

    Add Lab 1

--- concat.php
+++
@@ -1,6 +0,0 @@
-<?php
-$first = "Hello ";
-$second = "Bebra";
-$summ = $first . $second;
-echo $summ
-?>
--- variables.php
+++
@@ -1,7 +0,0 @@
-<?php
-$some_data = 15;
-if ($some_data % 2 == 0) {
-    echo "Число $some_data - парне";
-} else {
-    echo "Число $some_data - непарне";
-}
